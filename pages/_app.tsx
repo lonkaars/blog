12b41df69ad0bb3e2697071d55e9023026a0c59a
lonkaars <l.leblansch@gmail.com>
2021-03-26 20:59:39 +0100

    added fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,16 @@
+import Head from 'next/head';
+
 import '../styles/colors.css';
 import '../styles/layout.css';
 import '../styles/globals.css';
 import '../styles/navbarItem.css';
 
 export default function Blog({ Component, pageProps }) {
-	return <Component {...pageProps} />
+	return <div>
+		<Head>
+			<link rel="stylesheet" href="/font/font.css"/>
+		</Head>
+		<Component {...pageProps} />
+	</div>
 }
 
