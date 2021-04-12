f67a9d2caf5dea0993c920fdde5c7bcb61985ee9
lonkaars <l.leblansch@gmail.com>
2021-04-12 17:03:43 +0200

    added Discord embed + page title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,10 +11,20 @@ import '../styles/tags.css';
 import '../styles/card.css';
 
 export default function Blog({ Component, pageProps }) {
-	return <div>
+	return <>
 		<Head>
 			<link rel='stylesheet' href='/font/font.css' />
+
+			<title>Loek's Blog</title>
+
+			<meta property='og:site_name' content="Loek's blog" />
+			<meta property='og:url' content='https://blog.pipeframe.xyz' />
+			<meta property='og:title' content="Loek's excruciatingly interesting blog" />
+			<meta property='og:description' content="This is my personal blog website" />
+			<meta property='og:type' content='website' />
+			<meta name='theme-color' content='#e16d82' />
+
 		</Head>
 		<Component {...pageProps} />
-	</div>;
+	</>;
 }
