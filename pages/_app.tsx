e7742563d4737b087faa0cc11e4e93adfeca7650
lonkaars <loek@pipeframe.xyz>
2021-07-20 17:41:40 +0200

    new theme.css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,6 @@ import { useEffect, useState } from 'react';
 import '../styles/button.css';
 import '../styles/card.css';
 import '../styles/code.css';
-import '../styles/colors.css';
 import '../styles/globals.css';
 import '../styles/image.css';
 import '../styles/layout.css';
@@ -12,6 +11,7 @@ import '../styles/navbar.css';
 import '../styles/print.css';
 import '../styles/search.css';
 import '../styles/tags.css';
+import '../styles/theme.css';
 
 export default function Blog({ Component, pageProps }) {
 	var [dark, setDark] = useState(false);
