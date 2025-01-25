c318211f63576e728e90151994994da32526f286
Loek Le Blansch <loek@pipeframe.xyz>
2025-01-25 20:41:04 +0100

    use html hidden attribute instead of custom class

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 	let now = Date.now() / 1000;
 	document.querySelectorAll("[x-unhide-after]").forEach(el => {
 		if (now > el.getAttribute("x-unhide-after"))
-			el.classList.remove("hidden");
+			el.removeAttribute("hidden");
 	});
 }
 
